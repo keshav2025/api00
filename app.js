931a45541ce4d9aa5f832511d157857e2ffdd0a8
keshav2025 <keshavgupta86036@gmail.com>
2025-07-18 08:45:05 +0530

    change

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ app.get("/", (req, res) => {
 
 // ✅ Start server
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
